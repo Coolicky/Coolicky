d7382060e3bc1afb5c63313e505d6b5e86cf411f
Coolicky <piotr.kulicki@gmail.com>
2020-09-10 08:53:10 +0100

    changed hsts

--- Coolicky/Startup.cs
+++ Coolicky/Startup.cs
@@ -54,23 +54,11 @@ namespace Coolicky
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.Use(async (context, next) =>
-            {
-                if (context.Request.IsHttps)
-                {
-                    await next();
-                }
-                else
-                {
-                    var withHttps = "https://" + context.Request.Host + context.Request.Path;
-                    context.Response.Redirect(withHttps);
-                }
-            });
-
             app.UseHttpsRedirection();
+            app.UseHsts();
+
             app.UseStaticFiles();
 
             app.UseRouting();
