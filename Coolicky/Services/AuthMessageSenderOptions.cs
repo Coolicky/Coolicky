26a68865793f84c57b953a54d03ddeebd2536ef9
Coolicky <piotr.kulicki@gmail.com>
2020-09-13 14:25:47 +0100

    Variables Removed

--- Coolicky/Services/AuthMessageSenderOptions.cs
+++ Coolicky/Services/AuthMessageSenderOptions.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Coolicky.Services
+﻿namespace Coolicky.Services
 {
     public class AuthMessageSenderOptions
     {
